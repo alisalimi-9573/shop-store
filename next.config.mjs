63519d48a5621c76eacc0abcfa8da82422830e9b
Ali Salimi <ali.salimi7395@gmail.com>
2025-03-17 16:32:02 +0330

    add some needed to config next

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,16 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: false,
+// };
+
+// export default nextConfig;
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  output: "export",
+  basePath: "/shop-store",
+  assetPrefix: "/shop-store",
 };
 
 export default nextConfig;
