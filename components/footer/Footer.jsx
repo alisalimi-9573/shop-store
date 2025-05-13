8e6bc4a7a9269eb93fd5b7044740687ee59bf54c
Ali Salimi <ali.salimi7395@gmail.com>
2025-05-13 11:04:27 +0330

    footer styles

--- components/footer/Footer.jsx
+++ components/footer/Footer.jsx
@@ -4,7 +4,9 @@ import { Container } from "@mui/material";
 export default function Footer() {
   return (
     <>
-      <footer className="footer"></footer>
+      <footer className="footer">
+        <div>created by ali salimi</div>
+      </footer>
     </>
   );
 }
